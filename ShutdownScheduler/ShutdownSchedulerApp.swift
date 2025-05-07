81f70a038cc80f9f3250518130a5cd8b3f583758
hugang <hugang@codans.com>
2025-05-07 14:23:22 +0800

    Initial Commit

---
+++ ShutdownScheduler/ShutdownSchedulerApp.swift
@@ -0,0 +1,32 @@
+//
+//  ShutdownSchedulerApp.swift
+//  ShutdownScheduler
+//
+//  Created by Hu Gang on 2025/5/7.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ShutdownSchedulerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
